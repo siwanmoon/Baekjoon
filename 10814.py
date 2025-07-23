d7ebb0dfdfeff77bffe85120b755bcb2f42c5eb3
siwanmoon <simon___@naver.com>
2025-07-23 15:47:50 +0900

    10814

--- 10814.py
+++ 10814.py
@@ -4,7 +4,7 @@ member = []
 for _ in range(n):
     member.append(tuple(input().split()))
 
-member.sort(key = lambda x : (int(x[0]), x[1]))
+member.sort(key = lambda x : int(x[0]))
 
 for i in range(n):
     print(*member[i])
