0400bd22dd6b04c61fb00571dc113f4263084812
siwanmoon <simon___@naver.com>
2025-10-01 15:40:05 +0900

    27323

---
+++ Java/src/B27323.java
@@ -0,0 +1,15 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+
+public class B27323 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int a = Integer.parseInt(br.readLine());
+        int b = Integer.parseInt(br.readLine());
+
+        System.out.println(a * b);
+
+        br.close();
+    }
+}
