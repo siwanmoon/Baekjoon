80017b119a9c8f2b03df13097686efb11c20c8c4
siwanmoon <simon___@naver.com>
2025-10-08 21:10:06 +0900

    25311

---
+++ Java/src/B25311.java
@@ -0,0 +1,11 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+
+public class B25311 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println('A');
+        br.close();
+    }
+}
