1351109db4374372beb4e192c70ac56b45fe6177
siwanmoon <simon___@naver.com>
2026-01-15 16:35:16 +0900

    27889

---
+++ 27889.py
@@ -0,0 +1,10 @@
+name = input()
+
+if name == "NLCS":
+    print("North London Collegiate School")
+elif name == "BHA":
+    print("Branksome Hall Asia")
+elif name == "KIS":
+    print("Korea International School")
+elif name == "SJA":
+    print("St. Johnsbury Academy")
