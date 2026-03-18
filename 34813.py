4a2893ebc84b2ce00f529eee2f81a0df109b7174
siwanmoon <simon___@naver.com>
2026-03-18 14:30:05 +0900

    34813

---
+++ 34813.py
@@ -0,0 +1,11 @@
+n = input()
+
+match n[0]:
+    case "F":
+        print("Foundation")
+    case "C":
+        print("Claves")
+    case "V":
+        print("Veritas")
+    case "E":
+        print("Exploration")
